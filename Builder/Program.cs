cace25b993ef22cb09d468e639f997f5e0619975
Kuznetsov Daniil Andreevich <danikuznetsov@ozon.ru>
2021-10-04 12:13:47 +0300

    Proxy and builder first commit

---
+++ Builder/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+    
+    class Director
+    {
+        Builder builder;
+        public Director(Builder builder)
+        {
+            this.builder = builder;
+        }
+        public void Construct()
+        {
+            builder.BuildPartA();
+            builder.BuildPartB();
+            builder.BuildPartC();
+        }
+    }
+ 
+    abstract class Builder
+    {
+        public abstract void BuildPartA();
+        public abstract void BuildPartB();
+        public abstract void BuildPartC();
+        public abstract Product GetResult();
+    }
+ 
+    class Product
+    {
+        List<object> parts = new List<object>();
+        public void Add(string part)
+        {
+            parts.Add(part);
+        }
+    }
+ 
+    class ConcreteBuilder : Builder
+    {
+        Product product = new Product();
+        public override void BuildPartA()
+        {
+            product.Add("Part A");
+        }
+        public override void BuildPartB()
+        {
+            product.Add("Part B");
+        }
+        public override void BuildPartC()
+        {
+            product.Add("Part C");
+        }
+        public override Product GetResult()
+        {
+            return product;
+        }
+    }
+}
---
+++ Proxy/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Proxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+    
+    public interface IYoutubeApiClient
+    {
+        List<string> PopularVideo();
+        Video GetVideo(string id);
+    }
+    
+    public class ThpYoutubeApiClient : IYoutubeApiClient
+    {
+        public List<string> PopularVideo()
+        {
+            return new List<string>();
+        }
+
+        public Video GetVideo(string id)
+        {
+            return new Video();
+        }
+    }
+
+    public class CachedYoutubeApiClient : IYoutubeApiClient
+    {
+        private readonly ThpYoutubeApiClient _api;
+        private readonly Dictionary<string, Video> _set;
+
+        private CachedYoutubeApiClient()
+        {
+            _api = new ThpYoutubeApiClient();
+            _set = new Dictionary<string, Video>();
+        }
+        
+        public List<string> PopularVideo()
+        {
+            return new List<string>();
+        }
+
+        public Video GetVideo(string id)
+        {
+            if (_set.ContainsKey(id))
+            {
+                return _set[id];
+            }
+
+            var temp = _api.GetVideo(id);
+            _set.Add(id, temp);
+            return temp;
+        }
+    }
+
+    public class Video
+    {
+        public string Id { get; init; }
+        public string Title { get; init; }
+        public string Data { get; init; }
+
+        public Video(string id = null!, string title = null!, string data = null!)
+        {
+            Id = id ?? "435435";
+            Title = title ?? "Title";
+            Data = data ?? "Data";
+        }
+    }
+}
